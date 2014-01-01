42e684130705f3026266b238dae8402c287e5947
rmashal <ronen.mashal@gmail.com>
2014-01-01 08:49:00 +0200

    Supporting vertical focus shifts.

--- MagicSoftware.Common.Controls.DataGrid/Extensions/DataGridFocusManagementExtender.cs
+++ MagicSoftware.Common.Controls.DataGrid/Extensions/DataGridFocusManagementExtender.cs
@@ -5,27 +5,72 @@ using System.Text;
 using MagicSoftware.Common.Controls.Extensibility;
 using System.Windows.Controls;
 using System.Windows;
+using System.Windows.Input;
+using MagicSoftware.Common.Controls.Proxies;
 
 namespace MagicSoftware.Common.Controls.Table.Extensions
 {
    class DataGridFocusManagementExtender : ElementExtenderBase<DataGrid>
    {
+
+
+      public static IFocusManager GetFocusManager(DependencyObject obj)
+      {
+         return (IFocusManager)obj.GetValue(FocusManagerProperty);
+      }
+
+      public static void SetFocusManager(DependencyObject obj, IFocusManager value)
+      {
+         obj.SetValue(FocusManagerProperty, value);
+      }
+
+      // Using a DependencyProperty as the backing store for FocusManager.  This enables animation, styling, binding, etc...
+      public static readonly DependencyProperty FocusManagerProperty =
+          DependencyProperty.RegisterAttached("FocusManager", typeof(IFocusManager), typeof(DataGridFocusManagementExtender), new UIPropertyMetadata(null));
+
+
       protected EnhancedDGProxy DataGridProxy { get { return (EnhancedDGProxy)TargetElementProxy; } }
       protected ICurrentItemProvider CurrentItemTracker { get; private set; }
 
       protected override void Setup()
       {
          CurrentItemTracker = DataGridProxy.GetAdapter<ICurrentItemProvider>();
-         CurrentItemTracker.CurrentChanged += CurrentItemTracker_PropertyChanged;
+         CurrentItemTracker.PreviewCurrentChanging += CurrentItemTracker_PreviewCurrentChanging;
+         CurrentItemTracker.CurrentChanged += CurrentItemTracker_CurrentChanged;
       }
 
       protected override void Cleanup()
       {
-         CurrentItemTracker.CurrentChanged -= CurrentItemTracker_PropertyChanged;
+         CurrentItemTracker.PreviewCurrentChanging -= CurrentItemTracker_PreviewCurrentChanging;
+         CurrentItemTracker.CurrentChanged -= CurrentItemTracker_CurrentChanged;
       }
 
-      void CurrentItemTracker_PropertyChanged(object sender, RoutedEventArgs e)
+      ICurrentItemProvider previosLineCurrentItemContainer = null;
+
+      void CurrentItemTracker_PreviewCurrentChanging(object sender, CancelableRoutedEventArgs eventArgs)
       {
+         var currentItemContainer = DataGridProxy.CurrentItemContainer();
+         var currentItemContainerProxy = DataGridProxy.GetItemContainerProxy(currentItemContainer);
+         previosLineCurrentItemContainer = currentItemContainerProxy.GetAdapter<ICurrentItemProvider>();
+      }
+
+      void CurrentItemTracker_CurrentChanged(object sender, RoutedEventArgs e)
+      {
+         var currentItemContainer = DataGridProxy.CurrentItemContainer();
+         var currentItemContainerProxy = DataGridProxy.GetItemContainerProxy(currentItemContainer);
+         ICurrentItemProvider container = currentItemContainerProxy.GetAdapter<ICurrentItemProvider>();
+         if (previosLineCurrentItemContainer != null)
+         {
+            if (previosLineCurrentItemContainer.CurrentPosition > 0)
+               container.MoveCurrentToPosition(previosLineCurrentItemContainer.CurrentPosition);
+            else
+               container.MoveCurrentToPosition(0);
+         }
+         else
+         {
+            container.MoveCurrentToFirst();
+         }
+         
       }
    }
 }
--- MagicSoftware.Common.Controls.DataGrid/Extensions/EnhancedDGProxy.cs
+++ MagicSoftware.Common.Controls.DataGrid/Extensions/EnhancedDGProxy.cs
@@ -12,7 +12,7 @@ using System.Windows.Threading;
 
 namespace MagicSoftware.Common.Controls.Table.Extensions
 {
-   public class EnhancedDGProxy : ItemsControlProxy
+   public class ImprovedItemsControlProxy : ItemsControlProxy
    {
       #region Commands
 
@@ -29,22 +29,6 @@ namespace MagicSoftware.Common.Controls.Table.Extensions
 
       #endregion
 
-      public object ElementAsEventSource { get { return ProxiedElement; } }
-
-      private DataGrid DataGridElement { get { return (DataGrid)ProxiedElement; } }
-
-      public CollectionView ItemsView { get; private set; }
-
-      protected override void Initialize()
-      {
-         base.Initialize();
-      }
-
-      protected override void Cleanup()
-      {
-         base.Cleanup();
-      }
-
       public void AddRoutedEventHandler(RoutedEvent routedEvent, RoutedEventHandler handler)
       {
          ProxiedElement.AddHandler(routedEvent, handler);
@@ -54,6 +38,55 @@ namespace MagicSoftware.Common.Controls.Table.Extensions
       {
          ProxiedElement.RemoveHandler(routedEvent, handler);
       }
+
+
+      public object ElementAsEventSource { get { return ProxiedElement; } }
+
+      //TODO: Move to FrameworkElementProxy.
+      /// <summary>
+      /// Gets the proxied element's dispatcher.
+      /// </summary>
+      /// <returns>Dispatcher for UI operations.</returns>
+      internal Dispatcher GetDispatcher()
+      {
+         return this.ProxiedElement.Dispatcher;
+      }
+
+      internal void RaiseEvent(RoutedEventArgs eventArgs)
+      {
+         ProxiedElement.RaiseEvent(eventArgs);
+      }
+
+      public FrameworkElementProxy GetItemContainerProxy(object item)
+      {
+         var container = this.ContainerFromItem(item);
+         if (container == null)
+            return null;
+
+         return GetItemContainerProxy(container);
+      }
+
+      public FrameworkElementProxy GetItemContainerProxy(FrameworkElement container)
+      {
+         var proxy = FrameworkElementProxy.GetProxy(container);
+         if (proxy == null)
+         {
+            proxy = CreateItemContainerProxy(container);
+            FrameworkElementProxy.SetProxy(container, proxy);
+         }
+         return proxy;
+      }
+
+      protected virtual FrameworkElementProxy CreateItemContainerProxy(FrameworkElement itemContainer)
+      {
+         var itemContainerProxy = new FrameworkElementProxy();
+         return itemContainerProxy;
+      }
+   }
+
+   public class EnhancedDGProxy : ImprovedItemsControlProxy
+   {
+      private DataGrid DataGridElement { get { return (DataGrid)ProxiedElement; } }
 
       public override void ScrollIntoView(object item)
       {
@@ -86,19 +119,124 @@ namespace MagicSoftware.Common.Controls.Table.Extensions
          else return null;
       }
 
-      //TODO: Move to FrameworkElementProxy.
-      /// <summary>
-      /// Gets the proxied element's dispatcher.
-      /// </summary>
-      /// <returns>Dispatcher for UI operations.</returns>
-      internal Dispatcher GetDispatcher()
+      protected override FrameworkElementProxy CreateItemContainerProxy(FrameworkElement itemContainer)
       {
-         return this.ProxiedElement.Dispatcher;
-      }
-
-      internal void RaiseEvent(RoutedEventArgs eventArgs)
-      {
-         ProxiedElement.RaiseEvent(eventArgs);
+         if (itemContainer is DataGridRow)
+         {
+            return new ItemsContainerProxy();
+         }
+         return base.CreateItemContainerProxy(itemContainer);
       }
    }
+
+   class ItemsContainerProxy : FrameworkElementProxy
+   {
+      protected override void Initialize()
+      {
+         base.Initialize();
+      }
+
+      protected override object GetAdapter(Type adapterType)
+      {
+         if (ProxiedElement is DataGridRow)
+         {
+            if (adapterType == typeof(ICurrentItemProvider))
+               return new DataGridRowAsCurrentItemProvider(ProxiedElement as DataGridRow);
+         }
+         return base.GetAdapter(adapterType);
+      }
+   }
+
+   class DataGridRowAsCurrentItemProvider : ICurrentItemProvider
+   {
+      public event CancelableRoutedEventHandler PreviewCurrentChanging;
+
+      public event RoutedEventHandler CurrentChanged;
+      private DataGridRow dataGridRow;
+      private DataGrid owner;
+
+      public DataGridRowAsCurrentItemProvider(DataGridRow dataGridRow)
+      {
+         this.dataGridRow = dataGridRow;
+         owner = UIUtils.GetAncestor<DataGrid>(dataGridRow);
+      }
+
+      public object CurrentItem
+      {
+         get
+         {
+            if (owner.CurrentColumn == null)
+               return null;
+            return owner.CurrentColumn.GetCellContent(dataGridRow); 
+         }
+      }
+
+      public int CurrentPosition
+      {
+         get 
+         {
+            if (owner.CurrentColumn == null)
+               return -1;
+            return owner.CurrentColumn.DisplayIndex; 
+         }
+      }
+
+      bool MoveToCell(DataGridCell cell)
+      {
+         //HIGH: Should raise preview change event.
+
+         DataGridCellInfo newCellInfo = new DataGridCellInfo(cell);
+         owner.CurrentCell = newCellInfo;
+         return owner.CurrentCell.Column == cell.Column;
+
+         //HIGH: Should raise changed event.
+      }
+
+      public bool MoveCurrentTo(object item)
+      {
+         var cell = UIUtils.GetAncestor<DataGridCell>(item as UIElement);
+         return MoveToCell(cell);
+      }
+
+      public bool MoveCurrentToFirst()
+      {
+         return MoveCurrentToPosition(0);
+      }
+
+      public bool MoveCurrentToNext()
+      {
+         return MoveCurrentToRelativePosition(1);
+      }
+
+      public bool MoveCurrentToPrevious()
+      {
+         return MoveCurrentToRelativePosition(-1);
+      }
+
+      public bool MoveCurrentToLast()
+      {
+         return MoveCurrentToPosition(owner.Columns.Count - 1);
+      }
+
+      public bool MoveCurrentToPosition(int position)
+      {
+         var targetElement = owner.ColumnFromDisplayIndex(position).GetCellContent(dataGridRow);
+         return MoveCurrentTo(targetElement);
+      }
+
+      public bool MoveCurrentToRelativePosition(int offset)
+      {
+         int nextColumnIndex = 0;
+         var currentColumn = owner.CurrentColumn;
+         if (currentColumn != null)
+            nextColumnIndex = currentColumn.DisplayIndex + offset;
+         if (nextColumnIndex < 0)
+            nextColumnIndex = 0;
+         if (nextColumnIndex >= owner.Columns.Count)
+            nextColumnIndex = owner.Columns.Count - 1;
+         var targetElement = owner.ColumnFromDisplayIndex(nextColumnIndex).GetCellContent(dataGridRow);
+         return MoveCurrentTo(targetElement);
+      }
+   }
+
 }

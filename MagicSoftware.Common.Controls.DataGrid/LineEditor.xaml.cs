3c7bcdf775d0e8840f98889e7bb277ace2c1a423
rmashal <ronen.mashal@gmail.com>
2014-01-08 08:03:29 +0200

    First version of LineEditor

---
+++ MagicSoftware.Common.Controls.DataGrid/LineEditor.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace MagicSoftware.Common.Controls.Table
+{
+   /// <summary>
+   /// Interaction logic for LineEditor.xaml
+   /// </summary>
+   public partial class LineEditor : UserControl
+   {
+      public static readonly DependencyProperty ItemsSourceProperty = ItemsControl.ItemsSourceProperty.AddOwner(typeof(LineEditor));
+      public static readonly DependencyProperty ItemTemplateSelectorProperty = ItemsControl.ItemTemplateSelectorProperty.AddOwner(typeof(LineEditor), new FrameworkPropertyMetadata(new SelectTemplateByItemType()));
+
+      public IEnumerable ItemsSource
+      {
+         get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+         set { SetValue(ItemsSourceProperty, value); }
+      }
+
+      public DataTemplateSelector ItemTemplateSelector
+      {
+         get { return (DataTemplateSelector)GetValue(ItemTemplateSelectorProperty); }
+         set { SetValue(ItemTemplateSelectorProperty, value); }
+      }
+
+      public LineEditor()
+      {
+         InitializeComponent();
+      }
+
+      class SelectTemplateByItemType : DataTemplateSelector
+      {
+         public override DataTemplate SelectTemplate(object item, DependencyObject container)
+         {
+            FrameworkElement itemContainer = container as FrameworkElement;
+            var resource = itemContainer.TryFindResource(item.GetType());
+            if (resource != null)
+               return resource as DataTemplate;
+
+            return base.SelectTemplate(item, container);
+         }
+      }
+   }
+}

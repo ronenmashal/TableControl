a5342dcfc063e3363228ee566f12b7a3deffc676
rmashal <ronen.mashal@gmail.com>
2014-05-12 09:57:26 +0300

    Added logging to the tests.

---
+++ Tests.TableControl/AssemblyInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Common;
+using MagicSoftware.Common.Utils;
+
+namespace Tests.TableControl
+{
+   [TestClass]
+   public class AssemblyInitializer
+   {
+      [AssemblyInitialize()]
+      public static void InitializeAssembly(TestContext testContext)
+      {
+         TestLogUtils.InitializeLog(testContext);
+         // Force LoggingExtensions class initialization.
+         using (LoggingExtensions.Indent()) ;
+      }
+   }
+}

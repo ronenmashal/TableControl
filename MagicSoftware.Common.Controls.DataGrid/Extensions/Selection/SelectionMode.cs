1184e8c0bf567824ae3c353a40ac88150d35f14d
rmashal <ronen.mashal@gmail.com>
2014-07-19 19:49:40 +0300

    Reorganizing the selection manager.

---
+++ MagicSoftware.Common.Controls.DataGrid/Extensions/Selection/SelectionMode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using MagicSoftware.Common.Utils;
+using LogLevel = log4net.Core.Level;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace MagicSoftware.Common.Controls.Table.Extensions.Selection
+{
+   internal abstract class SelectionMode
+   {
+      protected ILog log = log4net.LogManager.GetLogger(DataGridSelectionExtender.LoggerName);
+
+      protected ICurrentItemService CurrentItemTracker { get; private set; }
+
+      protected MultiSelector Element { get; private set; }
+
+      public abstract void Enter();
+
+      public void Initialize(MultiSelector element, ICurrentItemService currentItemTracker)
+      {
+         Element = element;
+         CurrentItemTracker = currentItemTracker;
+      }
+
+      public abstract void Leave();
+
+      public abstract void OnCurrentItemChanged();
+
+      internal SelectionMode HandleInputEvent(object sender, InputEventArgs inputEventArgs)
+      {
+         if (inputEventArgs is KeyEventArgs)
+         {
+            return HandleKeyboardEvent(sender, inputEventArgs as KeyEventArgs);
+         }
+         else if (inputEventArgs is MouseButtonEventArgs)
+         {
+            return HandleMouseEvent(sender, inputEventArgs as MouseButtonEventArgs);
+         }
+         log.Debug("Don't know how to handle input event " + inputEventArgs);
+         return this;
+      }
+
+      protected abstract SelectionMode HandleKeyboardEvent(object sender, KeyEventArgs keyEventArgs);
+
+      protected abstract SelectionMode HandleMouseEvent(object sender, MouseButtonEventArgs mouseButtonEventArgs);
+   }
+}
---
+++ MagicSoftware.Common.Controls.DataGrid/Extensions/Selection/SingleSelectionMode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace MagicSoftware.Common.Controls.Table.Extensions.Selection
+{
+   internal class SingleSelectionMode : SelectionMode
+   {
+      public override void Enter()
+      {
+         log.Debug("Entering single selection mode");
+         Element.SelectedItem = CurrentItemTracker.CurrentItem;
+      }
+
+      public override void Leave()
+      {
+      }
+
+      public override void OnCurrentItemChanged()
+      {
+         Element.SelectedItem = CurrentItemTracker.CurrentItem;
+      }
+
+      protected override SelectionMode HandleKeyboardEvent(object sender, KeyEventArgs keyEventArgs)
+      {
+         if (keyEventArgs.IsDown)
+         {
+            if (SelectionModeManager.IsMultiSelectionKey(keyEventArgs.Key))
+            {
+               log.Debug("Pressed key is multi selection key.");
+               return SelectionModeManager.MultiSelectionMode;
+            }
+         }
+
+         return this;
+      }
+
+      protected override SelectionMode HandleMouseEvent(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+      {
+         return this;
+      }
+   }
+}

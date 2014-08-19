17f40911c33d181d82b62437862ad81e4312d3c4
rmashal <ronen.mashal@gmail.com>
2014-08-19 07:52:56 +0300

    Loading studio and opening a project.

--- MagicStudio/MainWindow.xaml.cs
+++ MagicStudio/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Windows;
+using com.magicsoftware.util;
 using log4net;
 using MagicSoftware.Studio;
 using MagicSoftware.Studio.Bridge;
+using MagicSoftware.Studio.Program;
+using MagicSoftware.Studio.Program.Logic;
+using MagicSoftware.Studio.Program.TaskItem;
+using MagicSoftware.Studio.Project;
 
 namespace MagicStudio
 {
@@ -11,12 +16,17 @@ namespace MagicStudio
    /// </summary>
    public partial class MainWindow : Window
    {
+      private Project currentProject;
       private ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
       public MainWindow()
       {
          InitializeComponent();
          InitializeStudioDll();
+
+         currentProject = new Project(ProjectManager.Instance);
+         currentProject.ProjectPath = @"d:\dev-magic\_Trunk\Sources\Projects\ggg\ggg.edp";
+         currentProject.Load();
       }
 
       protected void InitializeStudioDll()
@@ -28,6 +38,13 @@ namespace MagicStudio
             StudioDllWrapper.Initialize(new StudioDllImports());
             StudioInitializer.Initialize();
             StudioInitializer.InitializeConsumedAssemblies();
+
+            ProjectManager.CreateTaskFactory = CreateTaskFactory;
+            MainWindowHelper.HideMainWindow = HideMainWindow;
+
+            ProjectManager.GoToBreakInvoke = GoToBreakInvoke;
+            ProjectManager.CreateBreakInvoke = CreateBreakInvoke;
+            ProjectManager.CreateVariableChangeInvoke = CreateVariableChangeInvoke;
          }
          catch (System.Exception ex)
          {
@@ -41,8 +58,59 @@ namespace MagicStudio
 
       protected override void OnClosed(EventArgs e)
       {
-         base.OnClosed(e);
+         currentProject.Dispose();
          StudioDllWrapper.Instance.StudioDllClean();
+         base.OnClosed(e);
       }
+
+      #region Required for studio
+
+      public object CreateBreakInvoke(Logic logic, BrkLevel brkLevel, BrkType brkType, string text, bool isZoom, ReferencedKey reference)
+      {
+         TaskLine targetTaskLine = logic.CreateBreak(brkLevel, brkType, text, isZoom);
+
+         return targetTaskLine;
+      }
+
+      /// <summary>
+      /// create task factory
+      /// </summary>
+      /// <param name="program"></param>
+      /// <returns></returns>
+      public TaskFactory CreateTaskFactory(MagicSoftware.Studio.Program.Program program)
+      {
+         return new TaskFactory(program);
+      }
+
+      public object CreateVariableChangeInvoke(Logic logic, BrkLevel brkLevel, BrkType brkType, string text, bool isZoom, ReferencedKey reference)
+      {
+         return logic.CreateVariableChangeBreak(reference);
+      }
+
+      public void GoToBreakInvoke(MagicSoftware.Studio.Program.Task task, int handel, int taskLineIsn, int breakIsn)
+      {
+         //NavigationPath navigationPath = new NavigationPath();
+         //// navigate to task pane
+         //navigationPath.Add(new NavigateToTaskEditor(this, task.Program.RootTask.TaskHeader.Isn));
+         //// if needed, navigate to subtask
+         //if (!task.IsProgram)
+         //   navigationPath.Add(new NavigateToSubTask(new CreateTaskPathByIsn(task.Isn)));
+         //// navigate to logic tab
+         //navigationPath.Add(new NavigateInTabbedPresenter(MagicSoftware.Studio.Properties.Resources.LogicView_s));
+         //// navigate to task line
+         //navigationPath.Add(new NavigateToLine(new FindItemByISN(taskLineIsn)));
+
+         //navigationPath.Execute(Dispatcher.CurrentDispatcher);
+      }
+
+      /// <summary>
+      /// Hide the window
+      /// </summary>
+      public void HideMainWindow()
+      {
+         MainWindowHelper.Hide();
+      }
+
+      #endregion Required for studio
    }
 }
---
+++ MagicStudio/Project.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MagicSoftware.Common.ViewModel;
+using MagicSoftware.Common.Forms;
+using MagicSoftware.Studio.Project;
+using MagicSoftware.Studio.Repositories.Programs;
+using MagicSoftware.Studio.Forms;
+using MagicSoftware.Studio.Bridge;
+using System.Runtime.InteropServices;
+using MagicSoftware.Exp.Common.ViewModel;
+
+namespace MagicStudio
+{
+   public class Project : MagicSoftware.Exp.Common.ViewModel.ObservableObject
+   {
+      public static ObservableProperty ProjectPathProperty = new ObservableProperty(x => ((Project)x).ProjectPath, typeof(Project), "");
+      public static ObservableProperty IsLoadedProperty = new ObservableProperty(x => ((Project)x).IsLoaded, typeof(Project), false, ProjectPathProperty);
+      public static ObservableProperty CurrentPresenterProperty = new ObservableProperty(x=> ((Project)x).CurrentPresenter, typeof(Project), null);
+
+      private MagicSoftware.Studio.Project.ProjectManager projectManager;
+
+      public string ProjectPath
+      {
+         get
+         {
+            return (string)ProjectPathProperty.GetValue(this);
+         }
+         set
+         {
+            ProjectPathProperty.SetValue(this, value);
+         }
+      }
+
+      public bool IsLoaded
+      {
+         get
+         {
+            return (bool)IsLoadedProperty.GetValue(this);
+         }
+         private set
+         {
+            IsLoadedProperty.SetValue(this, value);
+         }
+      }
+
+      public PresenterBase CurrentPresenter
+      {
+         get { return (PresenterBase)CurrentPresenterProperty.GetValue(this); }
+         set { CurrentPresenterProperty.SetValue(this, value); }
+      }
+      
+
+      public static ObservableProperty SubTaskProperty = new ObservableProperty(x => ((Project)x).SubTask, typeof(Project), 2);
+        public int SubTask { 
+          get { 
+            return (int)SubTaskProperty.GetValue(this); 
+          } 
+          set { 
+            SubTaskProperty.SetValue(this, value);
+          }
+        }
+
+      public Project(MagicSoftware.Studio.Project.ProjectManager projectManager)
+      {
+         // TODO: Complete member initialization
+         this.projectManager = projectManager;
+      }
+
+      public bool Load()
+      {
+         StudioDllWrapper.MESSAGE_INFO messageResult = new StudioDllWrapper.MESSAGE_INFO();
+         var ReturnResult = ProjectManager.Instance.OpenProjectPhase1(ProjectPath);
+         ReturnResult = ProjectManager.Instance.OpenProjectPhase2(ProjectPath, ReturnResult);
+
+         messageResult = (StudioDllWrapper.MESSAGE_INFO)Marshal.PtrToStructure(ReturnResult, typeof(StudioDllWrapper.MESSAGE_INFO));
+         if (messageResult.Code != StudioDllWrapper.MESSAGE_CODE.CODE_SUCCESS)
+            return false;
+
+         IsLoaded = true;
+         return true;
+      }
+
+      public TaskHeader SelectProgram()
+      {
+         ISelectionDialog dlg = SelectionDialogFactory.CreateDialog(2, ZoomType.SelectProgram, null, null);
+         TaskHeader selectedTask = null;
+         if ((bool)dlg.ShowDialog())
+         {
+            selectedTask = projectManager.GetPrgByIdx((int)dlg.SelectedItem);
+         }
+         return selectedTask;
+      }
+
+   }
+}

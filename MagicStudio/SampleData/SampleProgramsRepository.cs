a26614bb9cd6a8e6efa5ca687d7570c73010adbb
rmashal <ronen.mashal@gmail.com>
2014-08-19 17:54:06 +0300

    Added folder style to the xaml and added design time sample data.

---
+++ MagicStudio/SampleData/SampleProgramsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagicStudio.SampleData
+{
+   internal class SampleProgramsRepository
+   {
+      public List<object> Programs { get; private set; }
+
+      public SampleProgramsRepository()
+      {
+         Programs = new List<object>();
+      }
+   }
+}
